8d23184654c4306f7e77b964e122bc4a0cf5a1bb
ren <blondor315@gmail.com>
2022-05-04 16:14:47 +0800

    add user_inquiry and multi_page

---
+++ stock_info_multi_page.py
@@ -0,0 +1,73 @@
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as html
+from  PIL import Image
+import numpy as np
+
+import pandas as pd
+from st_aggrid import AgGrid
+
+import io 
+
+
+with st.sidebar:
+    choose = option_menu("App Gallery", ["Search",  "Project Planning", "Python e-Course"],
+                         icons=['search',  'kanban', 'book'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
+
+
+if choose == "Search":
+    col1, col2 = st.columns( [0.8, 0.2])
+    with col1:               # To display the header text using css style
+        st.markdown(""" <style> .font {
+        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+        st.markdown('<p class="font">About the Creator</p>', unsafe_allow_html=True)    
+   
+        
+
+elif choose == "Project Planning":
+    col1, col2 = st.columns( [0.8, 0.2])
+    with col1:               # To display the header text using css style
+        st.markdown(""" <style> .font {
+        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+        st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
+        
+    
+        
+    #Add file uploader to allow users to upload photos
+    uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        
+        col1, col2 = st.columns( [0.5, 0.5])
+        with col1:
+            st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+            st.image(image,width=300)  
+
+        with col2:
+            st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+
+
+elif choose == "Python e-Course":
+    st.markdown(""" <style> .font {
+    font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+    </style> """, unsafe_allow_html=True)
+    st.markdown('<p class="font">Contact Form</p>', unsafe_allow_html=True)
+    with st.form(key='columns_in_form2',clear_on_submit=True): #set clear_on_submit=True so that the form will be reset/cleared once it's submitted
+        #st.write('Please help us improve!')
+        Name=st.text_input(label='Please Enter Your Name') #Collect user feedback
+        Email=st.text_input(label='Please Enter Email') #Collect user feedback
+        Message=st.text_input(label='Please Enter Your Message') #Collect user feedback
+        submitted = st.form_submit_button('Submit')
+        if submitted:
+            st.write('Thanks for your contacting us. We will respond to your questions or inquiries as soon as possible!')
---
+++ user_inquiry.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import streamlit as st
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0BwiihF-lw2kZo63LNIe8W11dKZfeaI8dciE3trlcnusbi7WAVxgXklDRaPQRpmvnrNrvYpnH6seb/pub?output=xlsx"
+
+@st.cache
+def get_list(name):
+    df = pd.read_excel(url,sheet_name = name,header=None)
+    return list(df[0])
+
+
+
+#建立各別的持有清單
+youren_owings = get_list("youren")
+pty_owings = get_list("pty")
+re_owings = get_list("re")
+cyc_owings = get_list("cyc")
+
+#建立持有的字典
+owings = dict(youren = youren_owings, pty = pty_owings, re = re_owings, cyc = cyc_owings) 
+
+text_input  = st.text_input('請輸入股號，多個股號以空白間隔')
+query = text_input.split()
+
+#query = input("輸入股號\n").split() #
+query = list(map(int, query))
+
+#產生比對結果的字典
+own_situation = {}
+
+for q in query:
+    result_list = []
+    for p,li in owings.items():
+        if q in li:
+            result_list.append(1)
+        else:
+            result_list.append(0)
+    own_situation[q] = result_list
+
+if text_input:
+    df = pd.DataFrame.from_dict(own_situation, orient='index',columns=['youren', 'pty', 're', 'cyc'])
+    df
+
