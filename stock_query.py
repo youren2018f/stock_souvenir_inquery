a084a792f95d0660a56a9adfff734518a4a1caff
ren <blondor315@gmail.com>
2022-05-06 09:04:04 +0800

    函數化

--- stock_query.py
+++ stock_query.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-###################################################
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+
+
 #從google sheet 讀取資料，並產生所有持股的字典
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0BwiihF-lw2kZo63LNIe8W11dKZfeaI8dciE3trlcnusbi7WAVxgXklDRaPQRpmvnrNrvYpnH6seb/pub?output=xlsx"
 @st.cache
@@ -14,42 +17,29 @@ re_owings = get_list("re")
 cyc_owings = get_list("cyc")
 
 owings = dict(youren = youren_owings, pty = pty_owings, re = re_owings, cyc = cyc_owings) 
-####################################################
 
 
 
-#5/2修改一下
-import pandas as pd
+
+#從hisotck 獲取資料
 website_path = "https://histock.tw/stock/gift.aspx"
-valid_stocks = pd.read_html(website_path,attrs = {'id': 'CPHB1_gvToday'})[0]
-#最新公佈的id為CPHB1_gvToday，未過最後買進的id為CPHB1_gv，已過最後買進的的id為CPHB1_gvOld
+@st.cache
+def histock_info(attr):
+    valid_stocks = pd.read_html(website_path,attrs = {'id': attr})[0]
+    #最新公佈的id為CPHB1_gvToday，未過最後買進的id為CPHB1_gv，已過最後買進的的id為CPHB1_gvOld
+    sub_stocks = valid_stocks[["代號", "名稱","股價","股東會紀念品"]]
+    new =sub_stocks.set_index("代號")
+    new["股東會紀念品"] = new["股東會紀念品"].str.replace("參考圖", "") #將最後的參考圖字樣去除
+    return new
 
 
-
-pd.options.display.max_columns = None
-pd.options.display.max_rows = None
-
-sub_stocks = valid_stocks[["代號", "名稱","股價","股東會紀念品"]]
-
-new =sub_stocks.set_index("代號")
-new["股東會紀念品"] = new["股東會紀念品"].str.replace("參考圖", "") #將最後的參考圖字樣去除
-
-
-check_list = []
-for i in range(len(sub_stocks)) :
-    check_list.append(sub_stocks.loc[i, "代號"])
-
-
-
-
-
-
-
+#從histock抓取要比對的股號清單
+info_detail = histock_info("CPHB1_gvOld")
+check_list = info_detail.index.values
 
 
 #產生持有情形的字典
 own_situation = {}
-
 
 for q in check_list:
     result_list = []
@@ -61,11 +51,14 @@ for q in check_list:
     own_situation[q] = result_list
             
 
+# 顯示結果
 df = pd.DataFrame.from_dict(own_situation, orient='index',columns=['youren', 'pty', 're', 'cyc'])
 
-df2 = pd.concat([new, df], axis=1) # axis=0 as default
-df2 = df2.round()
+st.write("資料如下")
+df2 = pd.concat([info_detail, df], axis=1) # axis=0 as default
 
+
+#st.table(df2)
 st.dataframe(df2)
 
  # Same as st.write(df)
